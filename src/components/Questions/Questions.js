066a7bf297cf54656dce75c06d414279a7cbf5ea
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-10-11 12:31:23 +0600

    correct ans icon also added

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Options from '../Options/Options';
 import swal from 'sweetalert';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 const Questions = ({ q }) => {
   const { question, id, correctAnswer, options, total } = q;
   // console.log(options);
   // console.log(correctAnswer);
+  const showCorrectAns = () => {
+    swal("Here the ans:", `${correctAnswer}`, "info")
+  }
   const ansHandle = (option) => {
     if (option === correctAnswer) {
       swal("Great!!", "Your answer is correct!", "success");
@@ -19,7 +24,8 @@ const Questions = ({ q }) => {
       <div className="max-w-3xl px-6 py-16 mx-auto space-y-12">
         <article className="space-y-8 rounded-lg dark:bg-gray-800 dark:text-gray-50">
           <div className="space-y-6">
-            <h1 className="text-4xl font-bold md:tracking-tight md:text-5xl p-4">{question}</h1>
+            <h1 className="text-4xl font-bold md:tracking-tight md:text-5xl p-4">{question} <button onClick={showCorrectAns}><FontAwesomeIcon icon={faEye}
+              className="text-yellow-500"></FontAwesomeIcon></button></h1>
           </div>
         </article>
         <div>
